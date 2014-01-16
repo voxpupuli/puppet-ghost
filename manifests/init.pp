b0b282dd7582c303234ee699925700291a94cd5f
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-01-15 23:45:37 -0800

    Modularizing manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -45,15 +45,19 @@ class ghost(
   $user             = 'ghost',
   $group            = 'ghost',
   $archive          = '/tmp/ghost.zip',
+  $source           = 'https://ghost.org/zip/ghost-latest.zip',
   $home             = '/opt/ghost',
-  $manage_nodejs    = true,
-  $autostart        = true,
-  $autorestart      = true,
-  $environment      = 'production',
+  $manage_nodejs    = true, # Install PPA and package
+  $use_supervisor   = true, # Use supervisor to manage Ghost
+  $autostart        = true, # Supervisor - Start at boot
+  $autorestart      = true, # Supervisor - Keep running
+  $environment      = 'production', # Supervisor - Ghost config environment to run
   $stdout_logfile   = '/var/log/supervisor/ghost.log',
   $stderr_logfile   = '/var/log/supervisor/ghost_err.log',
   $supervisor_conf  = '/etc/supervisor/conf.d/ghost.conf',
 
+  # Parameters below affect Ghost's config through the template
+  $manage_config    = true, # Manage Ghost's config.js
   $development_url  = 'http://my-ghost-blog.com',
   $development_host = '127.0.0.1',
   $development_port = 2368,
@@ -63,7 +67,7 @@ class ghost(
   $production_port  = 2368,
   ) {
 
-  if $manage_nodejs {
+  if $ghost::manage_nodejs {
     case $operatingsystem {
       'Ubuntu': {
         apt::ppa { 'ppa:chris-lea/node.js':
@@ -73,7 +77,9 @@ class ghost(
           ensure => latest,
         }
       }
-      default: {}
+      default: {
+        fail("${operatingsystem} is not supported, please fork and fix (or make an issue).")
+      }
     }
   }
 
@@ -95,13 +101,15 @@ class ghost(
   }
 
   wget::fetch { 'ghost':
-    source      => 'https://ghost.org/zip/ghost-latest.zip',
+    source      => $ghost::source,
     destination => $ghost::archive,
     before      => Exec['unzip_ghost'],
   }
 
-  package { [ 'unzip', 'supervisor' ]:
-    ensure => latest,
+  if ! defined( Package['unzip'] ) {
+    package { 'unzip':
+      ensure => present,
+    }
   }
 
   exec { 'unzip_ghost':
@@ -110,13 +118,14 @@ class ghost(
     require => [ Package['unzip'], File[$ghost::home] ],
   }
 
-  file { 'ghost_config':
-    path    => "${ghost::home}/config.js",
-    owner   => $ghost::user,
-    group   => $ghost::group,
-    content => template('ghost/config.js.erb'),
-    require => Exec['unzip_ghost'],
-    before  => Exec['start_ghost'],
+  if $ghost::manage_config {
+    file { 'ghost_config':
+      path    => "${ghost::home}/config.js",
+      owner   => $ghost::user,
+      group   => $ghost::group,
+      content => template('ghost/config.js.erb'),
+      require => Exec['unzip_ghost'],
+    }
   }
 
   exec { 'npm_install_ghost':
@@ -126,23 +135,32 @@ class ghost(
     require => [ Exec['unzip_ghost'], Package['nodejs'] ],
   }
 
-  exec { 'start_ghost':
-    command => '/usr/bin/supervisorctl start ghost',
-    require => [ Exec['npm_install_ghost'], Service['supervisor'] ],
-  }
+  if $ghost::manage_supervisor {
+    if ! defined( Package['supervisor'] ) {
+      package { 'supervisor':
+        ensure => present,
+      }
+    }
 
-  file { $ghost::supervisor_conf:
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    content => template('ghost/ghost.conf.erb'),
-    require => Package['supervisor'],
-  }
+    file { $ghost::supervisor_conf:
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      content => template('ghost/ghost.conf.erb'),
+      require => Package['supervisor'],
+    }
 
-  service { 'supervisor':
-    ensure    => running,
-    enable    => true,
-    require   => Package['supervisor'],
-    subscribe => File[$ghost::supervisor_conf],
+    service { 'supervisor':
+      ensure    => running,
+      enable    => true,
+      require   => Package['supervisor'],
+      subscribe => File[$ghost::supervisor_conf],
+    }
+
+    exec { 'start_ghost':
+      command => '/usr/bin/supervisorctl start ghost',
+      require => [ Exec['npm_install_ghost'],
+                   Service['supervisor'] ],
+    }
   }
 }
