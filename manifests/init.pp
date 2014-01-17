66e172dcefcefbc7e5cc7732722d2717db21a545
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-01-16 22:14:37 -0800

    Updating comments, moving archive

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,19 @@
 # == Class: ghost
 #
-# Full description of class ghost here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# This class installs the Ghost Blogging Platform.
 #
 # === Examples
 #
 #  class { ghost:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    $production_url = 'http://my-ghost-blog.com'
 #  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
 #
 # === Copyright
 #
-# Copyright 2014 Your name here, unless otherwise noted.
+# Copyright 2014 Andrew Schwartzmeyer
 #
-# TODO
+# === TODO
+#
 # - add database setup to template
 # - add mail setup to template
 # - support other operating systems
@@ -44,7 +22,7 @@
 class ghost(
   $user             = 'ghost',
   $group            = 'ghost',
-  $archive          = '/tmp/ghost.zip',
+  $archive          = '/opt/ghost.zip',
   $source           = 'https://ghost.org/zip/ghost-latest.zip',
   $home             = '/opt/ghost',
   $manage_nodejs    = true, # Install PPA and package
