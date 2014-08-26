84867595715cf6ba5c270ab163c778dc4ed3c087
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-08-26 11:32:51 -0700

    Closing issues #6, #15, and #16

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,3 +1,13 @@
+# == Class: ghost::setup
+#
+# This class includes nodejs if not already defined, and creates the
+# ghost user and group. It is not meant to be used directly, but
+# included from the base Ghost class.
+#
+# === Copyright
+#
+# Copyright 2014 Andrew Schwartzmeyer
+
 class ghost::setup {
 
   if ! defined(Class['nodejs']) { include nodejs }
@@ -12,11 +22,5 @@ class ghost::setup {
     home       => $ghost::home,
     managehome => true,
     require    => Group[$ghost::group],
-  }
-
-  exec { 'npm_config_set_registry':
-    command => "npm config set registry ${ghost::npm_registry}",
-    unless  => "npm config get registry | grep ${ghost::npm_registry}",
-    require => Class['nodejs'],
   }
 }
