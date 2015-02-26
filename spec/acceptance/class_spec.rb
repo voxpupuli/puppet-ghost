4bb441f99ba902428d6baa5cebed50009dbd4f81
Peter Souter <peter.souter@puppetlabs.com>
2015-02-26 16:55:36 +0000

    Adds class and acceptance tests

---
+++ spec/acceptance/class_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper_acceptance'
+
+describe 'ghost class' do
+
+  context 'default parameters' do
+    # Using puppet_apply as a helper
+    it 'should work idempotently with no errors' do
+      pp = <<-EOS
+      class { 'ghost': }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes  => true)
+    end
+
+    describe user('ghost') do
+      it { should exist }
+      it { should belong_to_group 'group' }
+    end
+
+    describe group('ghost') do
+      it { should exist }
+    end
+  end
+end
---
+++ spec/defines/blog_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+describe 'ghost::blog', :type => :define do
+  let :facts do {
+    :osfamily   => 'Debian',
+    :lsbdistid  => 'Debian',
+    :lsbrelease => 'wheezy',
+  } end
+
+  let(:title) { 'my_blog' }
+
+  describe "defaults" do
+    it {
+      should contain_exec("curl_ghost_my_blog")
+    }
+    it {
+      should contain_exec("unzip_ghost_my_blog")
+    }
+  end
+
+end
