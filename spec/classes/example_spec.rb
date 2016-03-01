8d208d86dcf9c115b23a443dece37a8ac41f34f1
Peter Souter <p.morsou@gmail.com>
2016-03-01 00:47:49 +0000

    Fixes rubocop errors
    Note: I don't agree with ` Avoid comma after the last item of a hash.` in `defines/blog_spec.rb:6`. Opening PR to change: https://github.com/voxpupuli/modulesync_config/pull/86

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -8,8 +8,8 @@ describe 'ghost' do
           facts
         end
 
-        context "ghost class without any parameters" do
-          let(:params) {{ }}
+        context 'ghost class without any parameters' do
+          let(:params) { {} }
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_user('ghost') }
           it { is_expected.to contain_group('ghost') }
