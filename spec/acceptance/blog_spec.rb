13b74a7fef702b398d76603ea50daf47a6e055d6
Dennis Hoppe <github@debian-solutions.de>
2016-04-21 15:34:41 +0200

    Use the new Ruby 1.9 hash syntax

--- spec/acceptance/blog_spec.rb
+++ spec/acceptance/blog_spec.rb
@@ -18,8 +18,8 @@ describe 'ghost class' do
       EOS
 
       # Run it twice and test for idempotency
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes  => true)
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
     end
 
     describe file('/home/ghost/my_blog') do
