c847b0f860660afc577d12151250db5e777df61b
Dennis Hoppe <github@debian-solutions.de>
2016-04-21 14:06:16 +0200

    Use the new Ruby 1.9 hash syntaxush

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -9,8 +9,8 @@ describe 'ghost class' do
       EOS
 
       # Run it twice and test for idempotency
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes  => true)
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
     end
 
     describe user('ghost') do
