372513c6cfdc2710c4ee2c5f51e4b0faedfc4c2f
Tim Meusel <tim@bastelfreak.de>
2022-07-01 12:43:49 +0200

    allow latest puppetlabs/apt in CI

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,5 +3,5 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  install_module_from_forge_on(host, 'puppetlabs-apt', '>= 4.1.0 < 5.0.0') if fact_on(host, 'os.family') == 'Debian'
+  install_module_from_forge_on(host, 'puppetlabs-apt', '>= 4.1.0 < 9.0.0') if fact_on(host, 'os.family') == 'Debian'
 end
