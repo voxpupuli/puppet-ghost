03c89da178dfb7416f4a87c985075e954c963fdb
Tim Meusel <tim@bastelfreak.de>
2022-07-01 12:40:38 +0200

    rubocop: autofix

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  if fact_on(host, 'os.family') == 'Debian'
-    install_module_from_forge_on(host, 'puppetlabs-apt', '>= 4.1.0 < 5.0.0')
-  end
+  install_module_from_forge_on(host, 'puppetlabs-apt', '>= 4.1.0 < 5.0.0') if fact_on(host, 'os.family') == 'Debian'
 end
